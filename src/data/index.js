4bd63c6e526b03b57171af153bbc8d181c6adec8
algomachine007 <amaben202phantom@gmail.com>
2022-11-24 11:52:49 +0100

    structuring data

---
+++ src/data/index.js
@@ -0,0 +1,13 @@
+export const headerLinks = [
+  { title: 'Services', url: '/', links: [{ title: 'Home', href: '/' }] },
+  { title: 'Products', url: '/', links: [{ title: 'Home', href: '/' }] },
+  { title: 'Markets', url: '/', links: [{ title: 'Home', href: '/' }] },
+  { title: 'Support', url: '/', links: [{ title: 'Home', href: '/' }] },
+  { title: 'Company', url: '/', links: [{ title: 'Home', href: '/' }] },
+];
+
+export const buttonHeader = { title: 'Contact Us', url: '/' };
+export const contactHeader = [
+  { title: '1 (800) 431-2335', icon: 'phone' },
+  { title: 'sales@adek', icon: 'mail' },
+];
