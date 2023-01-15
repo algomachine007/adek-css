68499496f4d2bd55bda773b9620b7cc1a204db77
algomachine007 <amaben202phantom@gmail.com>
2023-01-15 18:15:58 +0100

    Implementing the megamenu dropdowns

--- src/components/Navigation/BottomNavigation/Navigation.jsx
+++ src/components/Navigation/BottomNavigation/Navigation.jsx
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 
 import IconComponent from '../../../Icons';
 import Button from '../../Button/Button';
-import Dropdown from '../../HeaderElements/Dropdown/Dropdown';
+// import Dropdown from '../../HeaderElements/Dropdown/Dropdown';
 import { Hamburger } from '../../HeaderElements/Hamburger/Hamburger';
 
 import styles from './navigation.module.scss';
 
 const Navigation = ({ buttonHeader, headerLinks }) => {
   const [menu, setMenu] = useState(false);
+
+  console.log('HeaderLinks', headerLinks);
 
   const handleMenuToggle = () => setMenu((p) => !p);
 
@@ -17,19 +19,69 @@ const Navigation = ({ buttonHeader, headerLinks }) => {
       <nav className={styles.wrapper}>
         <IconComponent name='logo' />
         <ul>
-          {headerLinks.map(({ title, icon, subLinks }, index) => (
-            <li key={`li-${index}`}>
-              <span>
-                <p> {title} </p>
-                <IconComponent name={icon} />
-              </span>
-              <div className={styles.dropdown}>
+          {headerLinks.map(
+            ({ title, icon, content, contentMega, help }, index) => (
+              <li key={`li-${index}`}>
+                <span>
+                  <p> {title} </p>
+                  <IconComponent name={icon} />
+                </span>
+
+                {Array.isArray(content) && (
+                  <div>
+                    {content.map((elem) => (
+                      <div>
+                        <h3>{elem.title}</h3>
+                        <h4>{elem.details}</h4>
+                      </div>
+                    ))}
+                  </div>
+                )}
+
+                {Array.isArray(contentMega) && (
+                  <div>
+                    {contentMega.map((elem) => {
+                      console.log('ELEM', elem);
+                      return (
+                        <div>
+                          <h3>{elem.link}</h3>
+
+                          {!elem.isTwoColumn
+                            ? elem.menuItem.menu.map((elem) => {
+                                console.log('MegaMenu', elem);
+                                return (
+                                  <div>
+                                    {elem.link}
+                                    {elem.details}
+                                    {elem.title}
+                                  </div>
+                                );
+                              })
+                            : elem.menuItem.map((elem) => {
+                                return (
+                                  <div>
+                                    {elem.link}
+                                    {elem.details}
+                                    {elem.title}
+                                  </div>
+                                );
+                              })}
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+
+                {/* // create a component that when clicked would display the content */}
+
+                {/* <div className={styles.dropdown}>
                 {subLinks?.map(({ title, href }, index) => (
                   <Dropdown key={`sub-${index}`} link={title} url={href} />
                 ))}
-              </div>
-            </li>
-          ))}
+              </div> */}
+              </li>
+            )
+          )}
         </ul>
 
         <Button className={styles.button} title={buttonHeader.title} />
--- src/data/index.js
+++ src/data/index.js
@@ -1,42 +1,128 @@
+// Models to be built:
+// 1. ItemsWithDetail(s): This model would be used to create a list of items that have an Icon, Title and Description. They would be referenced in the ColumnsDropdowns (TwoColumnsDropdown & MegaColumnsDropdown )
+
+//2. Help Center model : This model would be comprised of: a. Icon, b. Text and c. Email fields.
+
+//3. HeaderLinks model: This model would be composed of:
+
+//4. megaDropdown model: This model would be composed of:
+
+// build a content dropdown
+const TwoColumnsDropdown = [
+  // reference the ItemsWithDetail model
+  {
+    icon: 'headphone',
+    title: 'Industrial Computer Repairs',
+    details:
+      'All industrial computer purchases made with ADEK receive a standard 2-year warranty and access to our technical support team throughout your systems’ lifetime. ADEK will repair most PC-based controllers on an industry-and-brand-agnostic basis, and provide you with a comprehensive repair or replacement plan.',
+  },
+  {
+    icon: 'headphone',
+    title: 'OEM Services',
+    details:
+      'All industrial computer purchases made with ADEK receive a standard 2-year warranty and access to our technical support team throughout your systems’ lifetime. ADEK will repair most PC-based controllers on an industry-and-brand-agnostic basis, and provide you with a comprehensive repair or replacement plan.',
+  },
+];
+
+const MegaColumnsDropdown = {
+  link: 'Computer Cases',
+  menu: [
+    // reference the ItemsWithDetail model
+    {
+      icon: 'headphone',
+      title: 'Computer Cases',
+      details: [
+        'Mid-Tower & Desktop Cases',
+        'Panel Mount Computer Cases',
+        'Rack Mount Computer Cases',
+      ],
+    },
+    {
+      icon: 'headphone',
+      title: 'Industrial Motherboards',
+      details: [
+        'ATX Motherboards with ISA Slots',
+        'ATX Motherboards with PCI Slots',
+        'Mini-ITX Industrial Motherboards',
+      ],
+    },
+    {
+      icon: 'headphone',
+      title: 'BackPlanes',
+      details: ['PICMG 1.0 Backplanes', 'PICMG 1.3 Backplanes'],
+    },
+    {
+      icon: 'headphone',
+      title: 'Single Board Computers',
+      details: [
+        'PICMG 1.0 Single Board Computers',
+        'PICMG 1.3 Single Board Computers',
+      ],
+    },
+  ],
+};
+
+const megaDropdown = [
+  {
+    link: 'Computer Cases',
+    menuItem: MegaColumnsDropdown,
+    isTwoColumn: false,
+  },
+  {
+    link: 'Embedded Systems',
+    // reference: Use the 2Columns model here
+    menuItem: TwoColumnsDropdown,
+    isTwoColumn: true,
+  },
+  {
+    link: 'Industrial Computers',
+    // reference: Use the 2Columns model here
+    menuItem: TwoColumnsDropdown,
+    isTwoColumn: true,
+  },
+  {
+    link: 'Panel PCs & Monitors',
+    // reference: Use the 2Columns model here
+    menuItem: TwoColumnsDropdown,
+    isTwoColumn: true,
+  },
+];
+
+const HelpCenter = {
+  icon: 'cube',
+  text: 'Do you need help?',
+  email: 'sales@adek.com',
+};
+
 export const headerLinks = [
   {
     title: 'Services',
     icon: 'chevron',
     url: '/',
-    subLinks: [
-      { title: 'Request Refund', href: '/' },
-      { title: 'Buy Service', href: '/' },
-      { title: 'Sell Service', href: '/' },
-    ],
+    // reference: Use the 2Columns model here
+    content: TwoColumnsDropdown,
+    help: HelpCenter,
   },
   {
     title: 'Products',
     icon: 'chevron',
     url: '/',
-    subLinks: [
-      { title: 'Buy Product', href: '/' },
-      { title: 'Sell Product', href: '/' },
-      { title: 'Request Refund', href: '/' },
-    ],
+    contentMega: megaDropdown,
+    help: HelpCenter,
   },
   {
     title: 'Markets',
-    url: '/',
+    url: '/markets',
   },
   {
     title: 'Support',
-    url: '/',
-  },
-  {
-    title: 'Company',
-    icon: 'chevron',
-    url: '/',
-    subLinks: [{ title: 'Home', href: '/' }],
+    url: '/support',
   },
 ];
 
 export const buttonHeader = { title: 'Contact Us', url: '/path' };
 
+// This should reference a generic content model
 export const contactHeader = [
   { title: '1 (800) 431-2335', icon: 'phone' },
   { title: 'sales@adek', icon: 'mail' },
