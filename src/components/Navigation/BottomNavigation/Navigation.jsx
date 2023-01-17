c148b7f70db80e5980a1f2f8deb30c46f1e1d00a
algomachine007 <amaben202phantom@gmail.com>
2023-01-17 09:59:09 +0100

    ensuring the dropdown is toggleable

--- src/components/Navigation/BottomNavigation/Navigation.jsx
+++ src/components/Navigation/BottomNavigation/Navigation.jsx
@@ -10,7 +10,19 @@ import styles from './navigation.module.scss';
 const Navigation = ({ buttonHeader, headerLinks }) => {
   const [menu, setMenu] = useState(false);
 
-  console.log('HeaderLinks', headerLinks);
+  const [toggleDropdown1, setToggleDropdown1] = useState(false);
+  const [toggleDropdown2, setToggleDropdown2] = useState(false);
+
+  const handleShowDropdowns = (index) => {
+    switch (index) {
+      case 0:
+        return setToggleDropdown1((p) => !p);
+      case 1:
+        return setToggleDropdown2((p) => !p);
+      default:
+        return null;
+    }
+  };
 
   const handleMenuToggle = () => setMenu((p) => !p);
 
@@ -23,52 +35,56 @@ const Navigation = ({ buttonHeader, headerLinks }) => {
             ({ title, icon, content, contentMega, help }, index) => (
               <li key={`li-${index}`}>
                 <span>
-                  <p> {title} </p>
+                  <p onClick={() => handleShowDropdowns(index)}>{title}</p>
                   <IconComponent name={icon} />
                 </span>
 
-                {Array.isArray(content) && (
-                  <div>
-                    {content.map((elem) => (
-                      <div>
-                        <h3>{elem.title}</h3>
-                        <h4>{elem.details}</h4>
-                      </div>
-                    ))}
-                  </div>
-                )}
+                <div>
+                  {Array.isArray(content) && (
+                    <div>
+                      SHOW ME
+                      {toggleDropdown1 &&
+                        content.map((elem) => (
+                          <div>
+                            <h3>{elem.title}</h3>
+                            <h4>{elem.details}</h4>
+                          </div>
+                        ))}
+                    </div>
+                  )}
+                </div>
 
                 {Array.isArray(contentMega) && (
                   <div>
-                    {contentMega.map((elem) => {
-                      console.log('ELEM', elem);
-                      return (
-                        <div>
-                          <h3>{elem.link}</h3>
-
-                          {!elem.isTwoColumn
-                            ? elem.menuItem.menu.map((elem) => {
-                                console.log('MegaMenu', elem);
-                                return (
-                                  <div>
-                                    {elem.link}
-                                    {elem.details}
-                                    {elem.title}
-                                  </div>
-                                );
-                              })
-                            : elem.menuItem.map((elem) => {
-                                return (
-                                  <div>
-                                    {elem.link}
-                                    {elem.details}
-                                    {elem.title}
-                                  </div>
-                                );
-                              })}
-                        </div>
-                      );
-                    })}
+                    SHOW ME
+                    {toggleDropdown2 &&
+                      contentMega.map((elem, index) => {
+                        console.log('IDX', index);
+                        return (
+                          <div>
+                            <h3>mega{elem.link}</h3>
+                            {!elem.isTwoColumn
+                              ? elem.menuItem.menu.map((elem) => {
+                                  return (
+                                    <div>
+                                      {elem.link}
+                                      {elem.details}
+                                      {elem.title}
+                                    </div>
+                                  );
+                                })
+                              : elem.menuItem.map((elem) => {
+                                  return (
+                                    <div>
+                                      {elem.link}
+                                      {elem.details}
+                                      <h2> {elem.title}</h2>
+                                    </div>
+                                  );
+                                })}
+                          </div>
+                        );
+                      })}
                   </div>
                 )}
 
